f374a31e003e9f66a7ea367bf747a6a1b0664539
Ryan <user@Ryans-MacBook-Pro.local>
2024-04-15 21:09:53 +0200

    ride share including pickup status

--- src/taxify/FindClosestServiceVehicle.java
+++ src/taxify/FindClosestServiceVehicle.java
@@ -7,8 +7,8 @@ public class FindClosestServiceVehicle implements IFindVehicle {
     public int getIndex(List<IVehicle> vehicles, ILocation origin) {
         int distance =  Integer.MAX_VALUE;
         int index = -1;
-        for (int vehicle=0; vehicle<vehicles.size()-1; vehicle++) {
-            if(vehicles.get(vehicle).getStatus() == VehicleStatus.SERVICE && (ApplicationLibrary.distance(vehicles.get(vehicle).getLocation(), origin) < distance)){
+        for (int vehicle=0; vehicle<vehicles.size(); vehicle++) {
+            if((vehicles.get(vehicle).getStatus() == VehicleStatus.SERVICE || vehicles.get(vehicle).getStatus() == VehicleStatus.PICKUP) && (ApplicationLibrary.distance(vehicles.get(vehicle).getLocation(), origin) < distance)){
                 System.out.println("made it within found service vehicle");
                 distance = ApplicationLibrary.distance(vehicles.get(vehicle).getLocation(), origin);
                 index = vehicle;
